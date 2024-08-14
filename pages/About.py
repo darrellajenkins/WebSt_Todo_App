a1ce3f6bb107cb8d2e802b2a4dee588620a68c95
Darrell <darrellajenkins@gmail.com>
2024-08-13 22:54:25 -0700

    Added new streamlit page: "About.py"

--- pages/About.py
+++ pages/About.py
@@ -5,20 +5,20 @@ import webbrowser
 # Sample additional website page.
 
 def goto():
-	search = st.session_state["search"]
-	webbrowser.open(f"https://duckduckgo.com/{search}?ia=web")
+    search = st.session_state["search"]
+    webbrowser.open(f"https://duckduckgo.com/{search}?ia=web")
 
 
 st.title("About")
 st.subheader("Would you like to learn more about the Developer?")
 select = st.radio('Select', options=['Other','Yes', 'No'])
 if select == 'Other':
-	st.write("You can now search the web. Enter your query below.")
-	st.text_input(label="Search:", on_change=goto, key="search")
+    st.write("You can now search the web. Enter your query below.")
+    st.text_input(label="Search:", on_change=goto, key="search")
 
 
 if select == 'Yes':
-	webbrowser.open('https://github.com/darrellajenkins')
+    webbrowser.open('https://github.com/darrellajenkins')
 st.write("\n\n\n\n")
 st.write(f"<b> {chr(169)} 2024 Inpyosis LLC<b/>",
          unsafe_allow_html=True)
